3eaa7453d8a76e22e9134f3249764c77e963f5b3
Karsten <karsten@mage.market>
2019-07-16 22:18:30 +0200

    build and jar

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 set mvn=C:\Program Files\JetBrains\IntelliJ IDEA 2018.3.4\plugins\maven\lib\maven3
+
 echo change maven if necessary
-java "-Dmaven.home=%mvn%" assembly:single
+
+"%JAVA_HOME%/bin/java" "-Dmaven.home=%mvn%" -classpath "%mvn%\boot\plexus-classworlds-2.5.2.jar" -Dmaven.multiModuleProjectDirectory=. "-Dclassworlds.conf=%mvn%\bin\m2.conf" org.codehaus.classworlds.Launcher -Didea.version=2018.3.4 -e clean compile assembly:single
